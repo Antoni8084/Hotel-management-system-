f1853e23b96c3d9519d666cc0548a297b66a3f15
Antoni Tanganyika <tanganyikaantoni1@gmail.com>
2025-01-07 18:32:47 +0300

    Initial

---
+++ h1.cxx
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "--- Hotel Management System ---\n";
+    cout << "1. Display Rooms\n";
+    cout << "2. Book Room\n";
+    cout << "3. Checkout Room\n";
+    cout << "4. Display Customer Details\n";
+    cout << "5. Exit\n";
+    cout << "Enter your choice: ";
+    int choice;
+    cin >> choice;
+    return 0;
+}
